f5b370542524d566429d782497d581281905a35d
joaop-oliveira <jpaulo2212gt@gmail.com>
2018-06-10 20:39:53 -0300

    AUTO COMMIT FROM PUSH.SH 2018-06-10 20:39:50

--- app/assets/javascripts/general.js
+++ app/assets/javascripts/general.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
     const tl = new TimelineLite();
     const card = document.querySelector('#cardGeneral');
-    const cardBody = document.querySelector('.card-body');
+    const aux = document.querySelector('#aux');
     const title = document.querySelector('#titleGeneral');
     const text = document.createTextNode('Hello General');
     tl.to(card, 1, {width: 1000, opacity: 1, ease:Bounce.easeOut, onComplete: function() {
       title.appendChild(text);
     }})
     .from(title, 1, {left: 600, opacity: 0})
-    .to(cardBody, 1, {bottom: 600, ease:Bounce.easeOut});
+    .to(aux, 1, {bottom: 600, ease:Bounce.easeOut});
 });
