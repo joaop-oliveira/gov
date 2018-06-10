1c2d12777112ee87f7dba8827d82afbad21c45ca
joaop-oliveira <jpaulo2212gt@gmail.com>
2018-06-10 19:57:09 -0300

    AUTO COMMIT FROM PUSH.SH 2018-06-10 19:57:07

---
+++ push.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+DATE=`date '+%Y-%m-%d %H:%M:%S'`
+echo "******** STARTING TO UPDATE REPO ********"
+echo "******** STARTING TO UPDATE REPO ********" >> .push_log.txt
+git pull >> push_log.txt
+echo "******** PULLED FROM REPO ********"
+echo "******** PULLED FROM REPO ********" >> push_log.txt
+git add . >> push_log.txt
+echo "******** STAGED ALL FILES ********"
+echo "******** STAGED ALL FILES ********" >> push_log.txt
+git commit -m "AUTO COMMIT FROM PUSH.SH ${DATE}" >> push_log.txt
+echo "******** COMMITED ALL THE FILES ********" &&
+echo "******** COMMITED ALL THE FILES ********" >> push_log.txt &7
+git push  >> push_log.txt
+echo "******** COMMITED ALL THE FILES ********" &&
+echo "******** COMMITED ALL THE FILES ********" >>  push_log.txt
