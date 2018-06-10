d9b9314b0dd3e47bf6d94f1337aaf62085ecf189
joaop-oliveira <jpaulo2212gt@gmail.com>
2018-06-10 20:20:34 -0300

    AUTO COMMIT FROM PUSH.SH 2018-06-10 20:20:31

--- app/assets/javascripts/general.js
+++ app/assets/javascripts/general.js
@@ -3,5 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const card = document.querySelector('#cardGeneral');
     const title = document.querySelector('#titleGeneral');
     const text = document.createTextNode('Hello General');
-    tl.to(card, 2, {width: 1000, opacity: 1})
-        .from(title, 1, {left: 600, opacity: 0});
+    tl.to(card, 2, {width: 1000, opacity: 1, onComplete: function() {
+     card.appendText(text);
+    }})
+    .from(title, 1, {left: 600, opacity: 0});
