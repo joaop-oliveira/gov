d3efd15ef08b67d7008c6f7a62caac69bc72d2e5
joaop-oliveira <jpaulo2212gt@gmail.com>
2018-06-10 21:26:24 -0300

    AUTO COMMIT FROM PUSH.SH 2018-06-10 21:26:22

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
   def flash_clas(key)
     case key
-      when "notice" then "alert alert-info"
-      when "alert" then "alert alert-danger"
+      when "notice" then "alert-new alert-info"
+      when "alert" then "alert-new alert-danger"
     end
   end
 end
