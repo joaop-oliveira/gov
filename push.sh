3d9c57ea2bce8871532f409c305e3350981af8ea
joaop-oliveira <jpaulo2212gt@gmail.com>
2018-06-11 12:02:26 -0300

    AUTO COMMIT FROM PUSH.SH 2018-06-11 12:02:25

--- push.sh
+++ push.sh
@@ -2,15 +2,15 @@
 
 DATE=`date '+%Y-%m-%d %H:%M:%S'`
 
-echo "******************************************" >> .push_log.txt
-echo "------------------------------------------" >> .push_log.txt
-echo "==========================================" >> .push_log.txt
-echo "******** STARTING TO UPDATE REPO ********"  >> .push_log.txt
-echo "******** STARTING TO UPDATE REPO ********"  >> .push_log.txt
-echo "*****************************************"  >> .push_log.txt
-echo "========================================="  >> .push_log.txt
+echo "******************************************" >> push_log.txt
+echo "------------------------------------------" >> push_log.txt
+echo "==========================================" >> push_log.txt
+echo "******** STARTING TO UPDATE REPO ********"  >> push_log.txt
+echo "******** STARTING TO UPDATE REPO ********"  >> push_log.txt
+echo "*****************************************"  >> push_log.txt
+echo "========================================="  >> push_log.txt
 
-git pull &> push_log.txt
+git pull >> push_log.txt
 
 echo "**********************************" >> push_log.txt
 echo "******** PULLED FROM REPO ********" >> push_log.txt
@@ -24,13 +24,13 @@ echo "******** STAGED ALL FILES ********" >> push_log.txt
 
 git commit -m "AUTO COMMIT FROM PUSH.SH ${DATE}" >> push_log.txt
 
-echo "******** COMMITED ALL THE FILES ********" &&
+echo "******** COMMITED ALL THE FILES ********" >> push_log.txt
 echo "******** COMMITED ALL THE FILES ********" >> push_log.txt
 
-git push | tee push_log.txt
+git push >> push_log.txt
 
-echo "******** PUSHING  ALL THE FILES ********" &&
-echo "******** PUSHING  ALL THE FILES ********" >>  push_log.txt
-echo "                                         " >> .push_log.txt
-echo "                                         " >> .push_log.txt
-echo "=========================================" >> .push_log.txt
+echo "******** PUSHING  ALL THE FILES ********"  >> push_log.txt
+echo "******** PUSHING  ALL THE FILES ********"  >> push_log.txt
+echo "                                         " >> push_log.txt
+echo "                                         " >> push_log.txt
+echo "=========================================" >> push_log.txt
