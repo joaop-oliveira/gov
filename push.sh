91c7c4b75731c3b38c14ebe7a4b6515c78226011
joaop-oliveira <jpaulo2212gt@gmail.com>
2018-06-11 12:06:37 -0300

    AUTO COMMIT FROM PUSH.SH 2018-06-11 12:06:35

--- push.sh
+++ push.sh
@@ -10,27 +10,44 @@ echo "******** STARTING TO UPDATE REPO ********"  >> push_log.txt
 echo "*****************************************"  >> push_log.txt
 echo "========================================="  >> push_log.txt
 
+echo "++++++++++++++++++++++++++++++++++++++++" >> push_log.txt
+
 git pull >> push_log.txt
+
+echo "++++++++++++++++++++++++++++++++++++++++" >> push_log.txt
 
 echo "**********************************" >> push_log.txt
 echo "******** PULLED FROM REPO ********" >> push_log.txt
 echo "******** PULLED FROM REPO ********" >> push_log.txt
 echo "----------------------------------" >> push_log.txt
 
+echo "++++++++++++++++++++++++++++++++++++++++" >> push_log.txt
+
 git add . >>  push_log.txt
+
+echo "++++++++++++++++++++++++++++++++++++++++" >> push_log.txt
 
 echo "******** STAGED ALL FILES ********"
 echo "******** STAGED ALL FILES ********" >> push_log.txt
 
+echo "++++++++++++++++++++++++++++++++++++++++" >> push_log.txt
+
 git commit -m "AUTO COMMIT FROM PUSH.SH ${DATE}" >> push_log.txt
+
+echo "++++++++++++++++++++++++++++++++++++++++" >> push_log.txt
 
 echo "******** COMMITED ALL THE FILES ********" >> push_log.txt
 echo "******** COMMITED ALL THE FILES ********" >> push_log.txt
+
+echo "++++++++++++++++++++++++++++++++++++++++" >> push_log.txt
 
 git push >> push_log.txt
 
+echo "++++++++++++++++++++++++++++++++++++++++" >> push_log.txt
+
+echo "========================================"  >> push_log.txt
 echo "******** PUSHING  ALL THE FILES ********"  >> push_log.txt
 echo "******** PUSHING  ALL THE FILES ********"  >> push_log.txt
-echo "                                         " >> push_log.txt
-echo "                                         " >> push_log.txt
+echo "****************************************"  >> push_log.txt
+echo "----------------------------------------"  >> push_log.txt
 echo "=========================================" >> push_log.txt
